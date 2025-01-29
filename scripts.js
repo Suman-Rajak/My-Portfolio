4f65e6f5de7cd2ba7ceda9889c39b90c7a98fc59
Suman Rajak <90976346+Suman-Rajak@users.noreply.github.com>
2025-01-30 00:52:33 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -74,22 +74,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Excel Sheet Connect
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxI9bMvwqnhUIiSuUk7Owfx8p6lXv8YgyHbnmUacamIV88j51FvwdRtElu6TuCphcmL/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg");
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbxI9bMvwqnhUIiSuUk7Owfx8p6lXv8YgyHbnmUacamIV88j51FvwdRtElu6TuCphcmL/exec'
+// const form = document.forms['submit-to-google-sheet']
+// const msg = document.getElementById("msg");
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = "Thank You for your response"
-            setTimeout(function () {
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+//         .then(response => {
+//             msg.innerHTML = "Thank You for your response"
+//             setTimeout(function () {
+//                 msg.innerHTML = ""
+//             }, 5000)
+//             form.reset()
 
-        }
+//         }
 
-        )
-        .catch(error => console.error('Error!', error.message))
-})
+//         )
+//         .catch(error => console.error('Error!', error.message))
+// })
+
